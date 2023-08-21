2d39f8bff17b82b55b0c3c0c141aa1fc771ac17e
Naushir Patuck <naush@raspberrypi.com>
2023-08-21 14:42:37 +0100

    meson: Add meson dist versioning support

--- utils/version.py
+++ utils/version.py
@@ -6,34 +6,45 @@
 import subprocess
 import sys
 from datetime import datetime
+from string import hexdigits
 
 digits = 12
 
 
 def generate_version():
     try:
-        # Check if this is a git directory
-        r = subprocess.run(['git', 'rev-parse', '--git-dir'],
-                            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, universal_newlines=True)
-        if r.returncode:
-            raise RuntimeError('Invalid git directory!')
-
-        # Get commit id
-        r = subprocess.run(['git', 'rev-parse', '--verify', 'HEAD'],
-                            stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, universal_newlines=True)
-        if r.returncode:
-            raise RuntimeError('Invalid git commit!')
-
-        commit = r.stdout.strip('\n')[0:digits]
-
-        # Check dirty status
-        r = subprocess.run(['git', 'diff-index', '--quiet', 'HEAD'],
-                            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, universal_newlines=True)
-        if r.returncode:
-            commit = commit + '-dirty'
-        
         if len(sys.argv) == 2:
-            commit = f'v{sys.argv[1]} {commit}'
+            # Check if this is a git directory
+            r = subprocess.run(['git', 'rev-parse', '--git-dir'],
+                                stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, universal_newlines=True)
+            if r.returncode:
+                raise RuntimeError('Invalid git directory!')
+
+            # Get commit id
+            r = subprocess.run(['git', 'rev-parse', '--verify', 'HEAD'],
+                                stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, universal_newlines=True)
+            if r.returncode:
+                raise RuntimeError('Invalid git commit!')
+
+            commit = r.stdout.strip('\n')[0:digits]
+
+            # Check dirty status
+            r = subprocess.run(['git', 'diff-index', '--quiet', 'HEAD'],
+                                stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, universal_newlines=True)
+            if r.returncode:
+                commit = commit + '-dirty'
+
+        elif len(sys.argv) == 3:
+            commit = sys.argv[2].lower().strip()
+            if any(c not in hexdigits for c in commit):
+                raise RuntimeError('Invalid git sha!')
+
+            commit = commit[0:digits]
+      
+        else:
+            raise RuntimeError('Invalid number of command line arguments') 
+
+        commit = f'v{sys.argv[1]} {commit}'
 
     except RuntimeError as e:
         print(f'ERR: {e}', file=sys.stderr)
