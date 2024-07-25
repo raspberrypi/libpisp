13b81f84474d630cf0cb85b37b2e76e710f183b1
Naushir Patuck <naush@raspberrypi.com>
2024-07-25 09:45:33 +0100

    tiling: Remove unused member variable from crop stage
    Also mark functions as override to keep clang happy.
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

--- src/libpisp/backend/tiling/crop_stage.hpp
+++ src/libpisp/backend/tiling/crop_stage.hpp
@@ -20,16 +20,15 @@ public:
 		Interval2 crop;
 	};
 	CropStage(char const *name, Stage *upstream, Config const &config, int struct_offset);
-	virtual Length2 GetOutputImageSize() const;
-	virtual void PushStartUp(int output_start, Dir dir);
-	virtual int PushEndDown(int input_end, Dir dir);
-	virtual void PushEndUp(int output_end, Dir dir);
-	virtual void PushCropDown(Interval interval, Dir dir);
+	virtual Length2 GetOutputImageSize() const override;
+	virtual void PushStartUp(int output_start, Dir dir) override;
+	virtual int PushEndDown(int input_end, Dir dir) override;
+	virtual void PushEndUp(int output_end, Dir dir) override;
+	virtual void PushCropDown(Interval interval, Dir dir) override;
 	bool BranchInactive() const override;
 
 private:
 	Config config_;
-	bool started_;
 };
 
 } // namespace tiling
