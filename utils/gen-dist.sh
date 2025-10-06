e933e97af25f473fabfc46db5fc1c825537f9adf
Naushir Patuck <naush@raspberrypi.com>
2025-10-06 09:32:06 +0100

    build: Simplify the version file generation
    Remove the gen-dist.sh script and do not add the git sha if we are doing
    and out-of-tree build. Instead, rely only on the version provided by the
    meson project.
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

--- utils/gen-dist.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# SPDX-License-Identifier: BSD-2-Clause
-# Copyright (C) 2023, Raspberry Pi Ltd
-
-cd "$MESON_SOURCE_ROOT" || return
-git rev-parse HEAD > "$MESON_DIST_ROOT"/version.gen
